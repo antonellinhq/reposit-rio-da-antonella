6509606e49ece61bd17896eec82e4f12d093e18f
antonellinhq <antonella.dalla.favera2011@gmail.com>
2025-09-30 10:03:56 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+numero1= float(input("digite um número"))
+numero8= float(input("digite outro número"))  
+if numero1 > numero8: 
+    print(numero1)
+elif numero1 == numero8:
+    print("são iguais")
+else:
+    print(numero8)
